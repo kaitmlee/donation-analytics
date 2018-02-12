3575ca9efb9a2afd251346940c40f482a508949c
kaitmlee <kaitmlee@gmail.com>
2018-02-12 17:13:35 -0600

    Update donation_analytics.py

--- src/donation_analytics.py
+++ src/donation_analytics.py
@@ -6,25 +6,25 @@ def scan(donors, percentile):
     #Add name/zipcode: first year to dict. 
     #check if they are there already update year if out of order
     lines = []
-    repeat_donors = dict()
+    unique_donors = dict()
     #Iterate through df to add all unique donors to list
     for index, row in donors.iterrows():
         #already in list
-        if (row["NAME"], row["ZIP_CODE"]) in repeat_donors:
+        if (row["NAME"], row["ZIP_CODE"]) in unique_donors:
             #if year2<year1 then update value in dict/drop row from df.
-            if(row["TRANSACTION_DT"]<repeat_donors[row["NAME"], row["ZIP_CODE"]]):
-                repeat_donors[row["NAME"], row["ZIP_CODE"]] = row["TRANSACTION_DT"]
+            if(row["TRANSACTION_DT"]<unique_donors[row["NAME"], row["ZIP_CODE"]]):
+                unique_donors[row["NAME"], row["ZIP_CODE"]] = row["TRANSACTION_DT"]
                 donors.drop(index, inplace=True)
             #otherwise there needs to be output
             else:
                 lines.append(output(donors, index, percentile))
         #not a repeat, add to list, drop row from df
         else: 
-            repeat_donors[row["NAME"], row["ZIP_CODE"]] = row["TRANSACTION_DT"]
+            unique_donors[row["NAME"], row["ZIP_CODE"]] = row["TRANSACTION_DT"]
             donors.drop(index, inplace=True)
     return lines
 
-def findPercentile(amounts, percentile):
+def find_percentile(amounts, percentile):
     #find the rank
     rank = int(((percentile/100) * len(amounts) + 0.5))
     if(rank==0):
@@ -46,24 +46,13 @@ def output(donors, index, percentile):
                 #add to list of donation amounts
                 amounts.append(row["TRANSACTION_AMT"])
         else: break
-    perc = findPercentile(amounts, percentile)
-    amt = sum(amounts)
+    perc = find_percentile(amounts, percentile)
+    amt = int(sum(amounts) + 0.5)
     cnt = len(amounts)
     return str("{0}|{1}|{2}|{3}|{4}|{5}".format(recip,zipcode,
                year,perc,amt,cnt))
 
-def main(argv):
-    itcont, perc, out = argv 
-    
-    percentile = int(open(perc, "r").read())
-    
-    donors = pd.read_csv(itcont, sep="|", 
-                         header=None,
-                         converters={10: lambda x: str(x),
-                                     13: lambda x: str(x)},
-                         usecols=[0,7,10,13,14,15])
-    donors.columns = ["CMTE_ID", "NAME", "ZIP_CODE", "TRANSACTION_DT",
-                      "TRANSACTION_AMT", "OTHER_ID"]
+def cleanData(donors):
     #OTHER_ID SHOULD BE NULL
     donors = donors[donors.OTHER_ID.isnull()]
     #DROP ROW IF ANY OTHER VALUE IS NULL
@@ -77,8 +66,24 @@ def main(argv):
     donors = donors[donors["TRANSACTION_DT"].apply(lambda x: len(str(x))==8)]
     #DATE IS CHANGED TO YYYY
     donors.TRANSACTION_DT = donors.TRANSACTION_DT.astype(str).str.slice(-4)
+    return donors
+
+def main(argv):
+    itcont, perc, out = argv 
+    
+    percentile = int(open(perc, "r").read())
+    
+    donors = pd.read_csv(itcont, sep="|", 
+                         header=None,
+                         converters={10: lambda x: str(x),
+                                     13: lambda x: str(x)},
+                         usecols=[0,7,10,13,14,15])
+    donors.columns = ["CMTE_ID", "NAME", "ZIP_CODE", "TRANSACTION_DT",
+                      "TRANSACTION_AMT", "OTHER_ID"]
+    donors = cleanData(donors)
 
     lines = scan(donors, percentile)
+
     with open(out, 'w') as output:
         output.write('\n'.join(lines))
     
