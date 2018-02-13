0d48e33cd935e6b21489d16bb5c7fd9b74c25f78
kaitmlee <kaitmlee@gmail.com>
2018-02-12 19:14:11 -0600

    Add files via upload

---
+++ src/test_donation_analytics.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 11 18:28:28 2018
+
+@author: kaitm
+"""
+import donation_analytics
+import unittest
+
+class TestDonationAnalytics(unittest.TestCase):
+    def test_find_percentile(self):
+        amounts = [1.1, 2.2, 3.5, 4.4, 5]
+        percentile = 80
+        expected = 4
+        actual = donation_analytics.find_percentile(amounts,percentile)
+        self.assertEqual(expected, actual)  
+    
+    def test_find_percentile(self):
+        amounts = [1.1, 2.2, 3.5, 4.5, 5]
+        percentile = 80
+        expected = 5
+        actual = donation_analytics.find_percentile(amounts,percentile)
+        self.assertEqual(expected, actual)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
